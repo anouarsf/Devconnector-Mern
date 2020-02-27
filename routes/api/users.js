19355e845170803f24230ed3cf28ae1313767bac
Lanker87 <anouarsfayhi@gmail.com>
2020-02-27 10:19:17 +0100

    user model , request & body validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require ('express-validator/check');
 
+// register/users
+router.post('/' ,[
+  check('name', 'Name is required')
+  .not()
+  .isEmpty(),
+  check('email', 'Please include valide email').isEmail(),
+  check('password', 'Please enter password with min 8 charch').isLength({min: 8}),
 
-// Get api/users
-router.get('/' , (req,res)=> res.send('User route'));
+], (req,res)=> {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+    return res.status(400).json ({ errors: errors.array() });
+  }
+  console.log(req.body);
+  res.send('User route');});
 
 module.exports = router;
