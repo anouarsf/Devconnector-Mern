d6c6ed9c58d1064093fa674247ca95e8577bc221
Lanker87 <anouarsfayhi@gmail.com>
2020-03-01 12:17:30 +0100

    create & update profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
 
@@ -21,5 +22,70 @@ res.json(profile);
 }
 
 });
+
+//route POSTE api/profile
+// CREATE OR UPDATE
+router.post('/', [auth, [
+  check('status', 'Status is required').not().isEmpty(),
+  check('skills', 'skills is required').not().isEmpty()
+]],
+async (req,res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+const {
+  website,
+  location,
+  status,
+  skills,
+  youtube,
+    facebook,
+    linkedin
+
+
+} = req.body;
+// Build profile
+const profileFields = {};
+
+profileFields.user = req.user.id;
+if (website)profileFields.website = website;
+if (location)profileFields.location = location;
+if (status)profileFields.status = status;
+if (skills) {
+  profileFields.skills = skills.split(',').map(skills =>skills.trim());
+}
+
+//build social object
+profileFields.social = {}
+if (youtube)profileFields.social.youtube = youtube;
+if (facebook)profileFields.social.facebook = facebook;
+if (linkedin)profileFields.social.linkedin = linkedin;
+
+try{
+
+let profile = await Profile.findOne ({ user : req.user.id}) ;
+if (profile) {
+  //Update
+  profile = await Profile.findOneAndUpdate ({ user:req.user.id} , {$set: profileFields},
+    {new: true}
+    );
+    return res.json(profile);
+}
+// Create
+profile = new Profile(profileFields);
+
+await profile.save();
+res.json(profile);
+}catch(err) {
+  console.error(err.message);
+  res.status(500).send('Server Error');
+}
+
+res.send('hello');
+
+}
+);
 
   module.exports = router;
