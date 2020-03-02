09677cb69bcd96fbe3a7eba312ab936f738b8afd
Lanker87 <anouarsfayhi@gmail.com>
2020-03-02 17:56:10 +0100

    react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Fragment} from 'react';
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Navbar from "./component/layout/Navbar";
+import Landing from "./component/layout/Landing";
+import Register from "./component/auth/Register";
+import Login from "./component/auth/Login";
+
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => 
+  <Router>
+
+<Fragment>
+<Navbar/>
+<Route exact path ="/" component={Landing} />
+<section>
+  <Switch>
+    <Route exact path="/register" component={Register}/>
+    <Route exact path="/login" component={Login}/>
+  </Switch>
+</section>
+</Fragment>
+
+</Router>
+
 
 export default App;
